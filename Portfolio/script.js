f38c972b7ace533138376ed06929b43fb41e1d56
Prasanjit-Biswas <115650462+Prasanjit-Biswas@users.noreply.github.com>
2022-10-17 19:40:16 +0530

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,22 @@
+
+
+
+// function sendMail() {
+//   let name = document.getElementById("name");
+//   let email = document.getElementById("email");
+//   let Phone = document.getElementById("Phone");
+//   let message = document.getElementById("message");
+
+//   let body = "name :" + name + "</br> email :" + email + "</br> phone :" + Phone + "</br message :>" + message;
+//   Email.send({
+//     Host: "smtp.elasticemail.com",
+//     Username: "biswaspiku274@gmail.com",
+//     Password: "AF65F461943BE07CB9F7416732AFA59086A9",
+//     To: 'tyou12948@gmail.com',
+//     From: "biswaspiku274@gmail.com",
+//     Subject: "This is the subject",
+//     Body: body
+//   }).then(
+//     message => alert(message)
+//   );
+// }
